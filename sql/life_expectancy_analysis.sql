b62fe9d936855970343c7186131710411f3787b0
JKenny333 <155671756+JKenny333@users.noreply.github.com>
2024-01-25 22:20:16 +0100

    updated sql queries

--- sql/life_expectancy_analysis.sql
+++ sql/life_expectancy_analysis.sql
@@ -32,108 +32,128 @@ USING(country_id);
 SELECT * FROM full_table;
 
 -- ================================================================================= --
-#Average life expectancy for entire world per year
+#General Life Expectancy Statistics
 
-DROP VIEW IF EXISTS avg_world_life_expectancy_per_year;
+	#Average life expectancy for entire world per year
+		DROP VIEW IF EXISTS avg_world_life_expectancy_per_year;
 
-CREATE VIEW avg_world_life_expectancy_per_year AS
-SELECT year, ROUND(AVG(life_expectancy),2) AS world_average_life_expectancy
-FROM life_expectancy_record
-GROUP BY year;
+		CREATE VIEW avg_world_life_expectancy_per_year AS
+		SELECT year, ROUND(AVG(life_expectancy),2) AS world_average_life_expectancy
+		FROM life_expectancy_record
+		GROUP BY year;
 
-SELECT * FROM avg_world_life_expectancy_per_year;
+		SELECT * FROM avg_world_life_expectancy_per_year;
+
+	#Average life expectancy by country in 2015
+		DROP VIEW IF EXISTS avg_life_expectancy_per_country_2015;
+
+		CREATE VIEW avg_life_expectancy_per_country_2015 AS
+		SELECT c.country, ROUND(AVG(life_expectancy), 2) AS average_life_expectancy_2015
+		FROM life_expectancy_record AS ler
+		JOIN country AS c ON ler.country_id = c.country_id
+		WHERE ler.year = 2015
+		GROUP BY c.country;
+
+		#top 10 counties for life expectancy
+			SELECT * FROM avg_life_expectancy_per_country_2015
+			ORDER BY average_life_expectancy_2015 DESC
+			Limit 10;
+
+		#bottom 10 countries for life expectancy
+			SELECT * FROM avg_life_expectancy_per_country_2015
+			ORDER BY average_life_expectancy_2015 ASC
+			Limit 10;
+
+		#Find your country's life expectancy
+			SELECT * FROM avg_life_expectancy_per_country_2015
+			WHERE country = "Ireland";
 
 -- ================================================================================= --
-#Average life expectancy by country in 2015
+#Health Factors
 
-DROP VIEW IF EXISTS avg_life_expectancy_per_country_2015;
+	#Alcohol consumptions effect on life expectancy
+		DROP VIEW IF EXISTS alcohol_consumption_life_expectancy;
 
-CREATE VIEW avg_life_expectancy_per_country_2015 AS
-SELECT c.country, ROUND(AVG(life_expectancy), 2) AS average_life_expectancy_2015
-FROM life_expectancy_record AS ler
-JOIN country AS c ON ler.country_id = c.country_id
-WHERE ler.year = 2015
-GROUP BY c.country;
+		CREATE VIEW alcohol_consumption_life_expectancy AS
+		(SELECT 
+			le.year, 
+			c.country, 
+			ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy, 
+			ROUND(AVG(le.alcohol), 2) AS avg_alcohol_consumption
+		FROM 
+			life_expectancy_record le
+		JOIN 
+			country c ON le.country_id = c.country_id
+		GROUP BY 
+			le.year, c.country);
 
-	#top 10 counties for life expectancy
-	SELECT * FROM avg_life_expectancy_per_country_2015
-	ORDER BY average_life_expectancy_2015 DESC
-	Limit 10;
+		SELECT * FROM alcohol_consumption_life_expectancy;
+        
+	#Correlation between happiness and life expectancy
+		DROP VIEW IF EXISTS happiness_life_expectancy;
 
-	#bottom 10 countries for life expectancy
-	SELECT * FROM avg_life_expectancy_per_country_2015
-	ORDER BY average_life_expectancy_2015 ASC
-	Limit 10;
-
-	#Find your country's life expectancy
-	SELECT * FROM avg_life_expectancy_per_country_2015
-	WHERE country = "Ireland";
+		CREATE VIEW happiness_life_expectancy AS
+		(SELECT 
+			country,
+			ROUND(AVG(happiness_score), 2) AS avg_happiness,
+			ROUND(AVG(life_expectancy), 2) AS avg_life_expectancy
+		FROM
+			country
+		JOIN
+			life_expectancy_record
+			USING(country_id)
+		JOIN 
+			happiness
+			USING(country_id)
+		GROUP BY country);
+		
+        #top 10 counties by happiness
+			SELECT * FROM happiness_life_expectancy
+			ORDER BY avg_happiness DESC
+			LIMIT 10;
+        
+        #bottom 10 counties by happiness
+			SELECT * FROM happiness_life_expectancy
+			ORDER BY avg_happiness ASC
+			LIMIT 10;
 
 -- ================================================================================= --
-#HEALTH FACTORS
+#Societal Factors
 
-#alcohol consumption compared to life expectancy
-DROP VIEW IF EXISTS alcohol_consumption_life_expectancy;
+	#GDP#s effect on life expectancy
+		DROP VIEW IF EXISTS gdp_life_expectancy;
 
-CREATE VIEW alcohol_consumption_life_expectancy AS
-(SELECT 
-    le.year, 
-    c.country, 
-    ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy, 
-    ROUND(AVG(le.alcohol), 2) AS avg_alcohol_consumption
-FROM 
-    life_expectancy_record le
-JOIN 
-    country c ON le.country_id = c.country_id
-GROUP BY 
-    le.year, c.country);
+		CREATE VIEW gdp_life_expectancy AS
+		(SELECT 
+			le.year, 
+			c.country, 
+			ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy, 
+			ROUND(AVG(le.gdp), 2) AS avg_gdp
+		FROM 
+			life_expectancy_record AS le
+		JOIN 
+			country c 
+			USING(country_id)
+		GROUP BY 
+			le.year, c.country);
 
-SELECT * FROM alcohol_consumption_life_expectancy
-
-
-#GDP compared to life expectancy
-DROP VIEW IF EXISTS gdp_life_expectancy;
-
-CREATE VIEW gdp_life_expectancy AS
-(SELECT 
-    le.year, 
-    c.country, 
-    ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy, 
-    ROUND(AVG(le.alcohol), 2) AS avg_alcohol_consumption
-FROM 
-    life_expectancy_record le
-JOIN 
-    country c ON le.country_id = c.country_id
-GROUP BY 
-    le.year, c.country);
-
-SELECT * FROM alcohol_consumption_life_expectancy
-
-#compare developed vs developing
-DROP VIEW IF EXISTS avg_world_life_expectancy_per_year_developed_vs_developiong;
-
-CREATE VIEW avg_world_life_expectancy_per_year_developed_vs_developiong AS
-SELECT year, ROUND(AVG(life_expectancy),2) AS world_average_life_expectancy
-FROM life_expectancy_record
-GROUP BY year;
-
-SELECT * FROM avg_world_life_expectancy_per_year;
+		SELECT * FROM gdp_life_expectancy
+		ORDER BY avg_gdp DESC
+		LIMIT 5;
 
 
-CREATE VIEW alcohol_consumption_life_expectancy AS
-(SELECT 
-    le.year, 
-    c.country, 
-    le.developed
-    ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy, 
-FROM 
-    life_expectancy_record le
-JOIN 
-    country c ON le.country_id = c.country_id
-GROUP BY 
-    le.year, c.country);
+	#Compare developed vs developing countries
+		DROP VIEW IF EXISTS developed_vs_developing;
+		
+		CREATE VIEW developed_vs_developing AS
+		(SELECT 
+            le.development_status,
+			ROUND(AVG(le.life_expectancy), 2) AS avg_life_expectancy
+		FROM 
+			life_expectancy_record AS le
+		GROUP BY 
+			le.development_status);
 
+		SELECT * FROM developed_vs_developing;
+        
 
-#GDP effect on life expectancy
-
-S
