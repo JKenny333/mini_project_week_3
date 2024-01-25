6e2e037adc3a6fccda9d06abb569525a28ec8f41
JKenny333 <155671756+JKenny333@users.noreply.github.com>
2024-01-25 17:24:53 +0100

    my sql queries

--- sql/life_expectancy_analysis.sql
+++ sql/life_expectancy_analysis.sql
@@ -2,4 +2,24 @@ USE life_expectancy;
 
 SHOW TABLES;
 
+SELECT * FROM life_expectancy_record;
+SELECT * FROM happiness;
 SELECT * FROM country;
+
+# Execute SQL queries for insights using commands (JOIN, GROUP BY, ORDER BY, CASE).
+
+#Average life expectancy for entire work in 2015
+DROP VIEW IF EXISTS avg_world_life_expectancy_per_year;
+
+CREATE VIEW avg_world_life_expectancy_per_year AS
+SELECT year, ROUND(AVG(life_expectancy),2) AS world_average_life_expectancy
+FROM life_expectancy_record
+GROUP BY year;
+
+SELECT * FROM avg_world_life_expectancy_per_year;
+
+#Average life expectancy by country in 2015
+CREATE VIEW avg_world_life_expectancy_per_country AS
+SELECT year, ROUND(AVG(life_expectancy),2)
+FROM life_expectancy_record
+GROUP BY year;
